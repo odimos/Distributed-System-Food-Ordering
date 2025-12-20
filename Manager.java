1b6c74f6346cceb785276c36f06ccb65ea9e18aa
odimos <dimosthenisdimoss@gmail.com>
2025-12-20 07:46:47 +0300

    readmy

--- Manager.java
+++ Manager.java
@@ -110,22 +110,22 @@ public class Manager implements ResponseHandler {
             //         )
             //     ),
 
-                // new Task(0, GlobalConfig.FILTER_STORES, false,
-                //     Map.of(
-                //         "category", "",
-                //         "stars", 0,
-                //         "price", "",
-                //         "latitude", 0.0,
-                //         "longitude", 0.0
-                //     )
-                // ),
+                new Task(0, GlobalConfig.FILTER_STORES, false,
+                    Map.of(
+                        "category", "",
+                        "stars", 0,
+                        "price", "",
+                        "latitude", 0.0,
+                        "longitude", 0.0
+                    )
+                ),
                 // new Task(9, GlobalConfig.GET_SALES_PER_PRODUCT, false, null),
                 // new Task(9, GlobalConfig.GET_SALES_PER_PRODUCT_CATEGORY, false,
                 //     Map.of("category", "Food")
                 // ),
-                new Task(9, GlobalConfig.GET_SALES_PER_FOOD_CATEGORY, false,
-                    Map.of("category", "Burger")
-                )
+                // new Task(9, GlobalConfig.GET_SALES_PER_FOOD_CATEGORY, false,
+                //     Map.of("category", "Burger")
+                // )
          });
 
         
--- readme.md
+++ readme.md
@@ -1,9 +1,19 @@
-Λειτουργίες manager:
-● Να προσθέτουν καταστήματα
-● Να προσθέτουν/αφαιρούν διαθέσιμα προϊόντα
-● Να προσθέτουν νέα προϊόντα ή να αφαιρούν παλιά
-● Να εμφανίζουν τις συνολικές πωλήσεις ανα προϊόν
-● Η διαχείριση μπορεί να γίνεται μέσω console application
 
-Manager Console App 
-Πελάτης: Dummy Εφαρμογή
+Master (Server) <-> (Client) Manager, App
+Master (Client) <-> (Server) Workers
+Master (Server) <-> (Client) Reducer 
+Reducer (Server) <-> (Client) Workers
+Reducer (Server) <-> (Client) Workers
+
+Ολες οι υλοποιήσεις της σχέσης client - server
+γίνονται με την κλάση Client και την κλάση Server
+Και στις δύο περιπτώσεις, προκειμένου να διαχιερίζεται την απάντηση αποτελεσματικά το αντικείμενο, 
+υπάρχει ο όρος <M extends ResponseHandler> .
+Οπότε κάθε αντικείμενο που θα καλέσει την σχέση client - server public Client( Task task, String host, int port, M manager)
+θα πρέπει να υλοποιεί και την συνάρτηση responseHandler ή requestHandler, η οποία θα καθορίζει πως διαχειρίζεται την απάντηση που δέχεται.
+
+Οι συνδέσεις πραγματοποιούνται με TCP sockets σε νήματα (Threads)
+
+
+
+![Schema](res/schema.jpg)
